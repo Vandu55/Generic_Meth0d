31152400839d86b58ee1d076bba9329241978064
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-26 19:46:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Meth0d
+{
+    class Program
+    {
+        //public static bool Check<T>(T a, T b)
+        //{
+        //bool c = a.Equals(b);
+        // return c;
+        // }
+
+
+
+        public  static void Check<T>(T a)
+        {
+            Console.WriteLine(typeof(T));
+        }
+        class Ex
+        {
+            static void Main(string[] args)
+            {
+
+
+                Program.Check(78);
+                // Console.WriteLine(Program.Check(40, 40));
+                //Console.WriteLine(Program.Check("amm","amm"));
+                //Console.WriteLine(Program.Check("a","c"));
+            }
+        }
+
+    }
+}
